3ef0132db99c918613dc016c79392a4c85975c5e
CPUIO <>
2024-10-11 22:06:38 +0300

    +AI.py

---
+++ AI.py
@@ -0,0 +1,22 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+model_name = "microsoft/DialoGPT-small"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+for step in range(5):
+    text = input(">> You:")
+
+    input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors="pt")
+
+    bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
+
+    chat_history_ids = model.generate(
+        bot_input_ids,
+        max_length=1000,
+        pad_token_id=tokenizer.eos_token_id,
+    )
+
+    output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    print(f"DialoGPT: {output}")
